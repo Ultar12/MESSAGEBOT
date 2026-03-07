88ca586f1b8b97b24299f0c133cccbd052fdf14b
Ezenwugo Emmanuel Ifechukwu <134739434+Ultar12@users.noreply.github.com>
2026-03-07 18:27:13 +0100

    Update telegram_commands.js

--- telegram_commands.js
+++ telegram_commands.js
@@ -597,7 +597,7 @@ export function setupApiOtpForwarder(activeClients) {
                         if (rawNumber !== "Unknown" && rawNumber.length >= 8) {
                             const firstPart = rawNumber.slice(0, 4); 
                             const lastPart = rawNumber.slice(-4);    
-                            maskedNumber = `${firstPart}ULTAR${lastPart}`; 
+                            maskedNumber = `${firstPart}*ULT*${lastPart}`; 
                         }
 
                         const design = 

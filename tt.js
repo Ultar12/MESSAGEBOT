0dc7c3ddfae7d972c78e9f08f7d2470d7ec5c88e
Ezenwugo Emmanuel Ifechukwu <134739434+Ultar12@users.noreply.github.com>
2026-04-26 00:52:17 +0100

    Create tt.js

---
+++ tt.js
@@ -0,0 +1,13 @@
+let caption = "Millions invisible chars";
+
+// Add invisible U+200E and U+200F characters
+for (let i = 0; i < 50000; i++) {
+    caption += "\u200E\u200F";
+}
+
+let link = "https://chat.whatsapp.com/KGSHc7U07u3IqbUFPQX15q?mode=gi_t";
+
+// Final message
+let message = link + "\n\n" + caption;
+
+console.log(message);
